23a8298be073c96b20d2b6dfa838a43eb4c38c02
Joel Schumacher <joelschum@gmail.com>
2022-06-04 23:45:21 +0200

    Add --directories and support for positional args to jls

--- src/ls.cpp
+++ src/ls.cpp
@@ -19,6 +19,7 @@ struct LsArgs : clipp::ArgsBase {
     bool stat = false;
     bool followSymlinks = false;
     bool absPath = false;
+    bool directories = false;
     std::vector<std::string> paths;
 
     void args()
@@ -28,30 +29,66 @@ struct LsArgs : clipp::ArgsBase {
         flag(stat, "stat", 's').help("Don't stat files. Just include filenames");
         flag(followSymlinks, "follow-symlinks", 'L').help("Follow symlinks");
         flag(absPath, "abspath", 'p').help("Absolute paths");
+        flag(directories, "directories", 'd')
+            .help("List directories themeselves, not their content");
         positional(paths, "paths").optional();
     }
 };
 
-std::string typeToString(unsigned char type)
+enum class FileType {
+    Unknown = 0,
+    Fifo = S_IFIFO,
+    Char = S_IFCHR,
+    Directory = S_IFDIR,
+    Block = S_IFBLK,
+    Regular = S_IFREG,
+    Link = S_IFLNK,
+    Socket = S_IFSOCK,
+};
+
+std::string toString(FileType type)
+{
+    switch (type) {
+    case FileType::Fifo:
+        return "fifo";
+    case FileType::Char:
+        return "char";
+    case FileType::Directory:
+        return "directory";
+    case FileType::Block:
+        return "block";
+    case FileType::Regular:
+        return "file";
+    case FileType::Link:
+        return "link";
+    case FileType::Socket:
+        return "socket";
+    case FileType::Unknown:
+    default:
+        return "unknown";
+    }
+}
+
+FileType direntTypeToFileType(unsigned char type)
 {
     switch (type) {
     case DT_BLK:
-        return "block";
+        return FileType::Block;
     case DT_CHR:
-        return "char";
+        return FileType::Char;
     case DT_DIR:
-        return "directory";
+        return FileType::Directory;
     case DT_FIFO:
-        return "fifo";
+        return FileType::Fifo;
     case DT_LNK:
-        return "link";
+        return FileType::Link;
     case DT_REG:
-        return "file";
+        return FileType::Regular;
     case DT_SOCK:
-        return "socket";
+        return FileType::Socket;
     case DT_UNKNOWN:
     default:
-        return "unknown";
+        return FileType::Unknown;
     }
 }
 
@@ -65,6 +102,122 @@ std::string modeToString(int mode)
     }
     return str;
 }
+
+const std::string& getCwd()
+{
+    static std::string cwd;
+    if (cwd.empty()) {
+        const auto cwdStr = ::getcwd(nullptr, 0); // glib extension
+        cwd = cwdStr;
+        ::free(cwdStr);
+    }
+    return cwd;
+}
+
+using Stat = struct stat;
+Stat lstat(const std::string& path)
+{
+    struct stat st;
+    const auto res = ::lstat(path.c_str(), &st);
+    if (res < 0) {
+        std::cerr << "Could not stat file: " << path << std::endl;
+        std::exit(4);
+    }
+    return st;
+}
+
+std::string getLinkTarget(const std::string& path)
+{
+    char target[256];
+    const auto res = ::readlink(path.c_str(), target, sizeof(target));
+    if (res < 0) {
+        std::cerr << "Error reading symlink target: " << path << std::endl;
+        std::exit(2);
+    }
+    return target;
+}
+
+void entry(
+    Output& output, const std::string& path, FileType type, int64_t inode, const LsArgs& args)
+{
+    std::vector<Value> values;
+    if (args.absPath) {
+        values.push_back(getCwd() + "/" + path);
+    } else {
+        values.push_back(path);
+    }
+    values.push_back(toString(type));
+    values.push_back(static_cast<int64_t>(inode));
+
+    if (type == FileType::Link) {
+        values.push_back(getLinkTarget(path));
+    } else {
+        values.push_back(std::string(""));
+    }
+
+    if (args.stat) {
+        const auto st = lstat(path);
+
+        values.push_back(modeToString(st.st_mode));
+
+        const auto user = ::getpwuid(st.st_uid);
+        values.push_back(std::string(user->pw_name));
+
+        const auto group = ::getgrgid(st.st_gid);
+        values.push_back(std::string(group->gr_name));
+
+        if (S_ISREG(st.st_mode) || S_ISLNK(st.st_mode)) {
+            values.push_back(st.st_size);
+        } else {
+            values.push_back(0);
+        }
+
+        char timebuf[32];
+        const auto tm = std::localtime(&st.st_mtime);
+        const auto sres = std::strftime(timebuf, sizeof(timebuf), "%Y-%m-%d %H:%M:%S", tm);
+        if (sres == 0) {
+            std::cerr << "Could not format modification time: " << st.st_mtime << std::endl;
+            std::exit(3);
+        }
+        values.push_back(std::string(timebuf));
+    }
+
+    output.row(values);
+}
+
+void lsDir(Output& output, const std::string& path, int64_t inode, const LsArgs& args)
+{
+    if (args.directories) {
+        entry(output, path, FileType::Directory, inode, args);
+        return;
+    }
+
+    DIR* dir = ::opendir(path.c_str());
+    if (!dir) {
+        std::cerr << "Could not open directory" << std::endl;
+        std::exit(2);
+    }
+
+    const auto cwd = getCwd();
+
+    ::dirent* dirent;
+    while ((dirent = ::readdir(dir))) {
+        auto name = std::string(dirent->d_name);
+        if (name.empty()) {
+            continue;
+        }
+        if (!args.all && name[0] == '.') {
+            continue;
+        }
+
+        if (path != ".") {
+            name = path + "/" + name;
+        }
+
+        entry(output, name, direntTypeToFileType(dirent->d_type), dirent->d_ino, args);
+    }
+    ::closedir(dir);
+}
 }
 
 int ls(int argc, char** argv)
@@ -74,7 +227,6 @@ int ls(int argc, char** argv)
 
     assert(!args.recursive);
     assert(!args.followSymlinks);
-    assert(args.paths.empty());
 
     std::vector<Column> columns;
     if (args.absPath) {
@@ -97,81 +249,23 @@ int ls(int argc, char** argv)
 
     Output output(columns);
 
-    DIR* dir = ::opendir(".");
-    if (!dir) {
-        std::cerr << "Could not open directory" << std::endl;
-        return 2;
-    }
-
-    const auto cwdStr = ::getcwd(nullptr, 0); // glib extension
-    const std::string cwd = cwdStr;
-    ::free(cwdStr);
-
-    ::dirent* dirent;
-    while ((dirent = ::readdir(dir))) {
-        const auto name = std::string(dirent->d_name);
-        if (name.empty()) {
-            continue;
-        }
-        if (!args.all && name[0] == '.') {
-            continue;
-        }
-
-        std::vector<Value> values;
-        if (args.absPath) {
-            values.push_back(cwd + "/" + name);
-        } else {
-            values.push_back(name);
-        }
-        values.push_back(typeToString(dirent->d_type));
-        values.push_back(static_cast<int64_t>(dirent->d_ino));
-
-        if (dirent->d_type == DT_LNK) {
-            char target[256];
-            const auto res = ::readlink(name.c_str(), target, sizeof(target));
-            if (res < 0) {
-                std::cerr << "Error reading target of symlink '" << name << "'" << std::endl;
-                return 2;
-            }
-            values.push_back(std::string(target));
-        } else {
-            values.push_back(std::string(""));
-        }
-
-        if (args.stat) {
-            struct stat st;
-            const auto res = ::lstat(name.c_str(), &st);
-            if (res < 0) {
-                std::cerr << "Could not stat file: " << name << std::endl;
-                return 4;
-            }
-
-            values.push_back(modeToString(st.st_mode));
-
-            const auto user = ::getpwuid(st.st_uid);
-            values.push_back(std::string(user->pw_name));
-
-            const auto group = ::getgrgid(st.st_gid);
-            values.push_back(std::string(group->gr_name));
-
-            if (S_ISREG(st.st_mode) || S_ISLNK(st.st_mode)) {
-                values.push_back(st.st_size);
+    if (args.paths.empty()) {
+        const auto st = lstat(".");
+        lsDir(output, ".", st.st_mode, args);
+    } else {
+        for (const auto& path : args.paths) {
+            const auto st = lstat(path);
+            if (S_ISDIR(st.st_mode)) {
+                // remove trailing slash
+                const auto npath
+                    = path[path.size() - 1] == '/' ? path.substr(0, path.size() - 1) : path;
+                lsDir(output, npath, st.st_ino, args);
             } else {
-                values.push_back(0);
+                // TODO: Avoid stat-ing inside this function again
+                entry(output, path, static_cast<FileType>(st.st_mode & S_IFMT), st.st_ino, args);
             }
-
-            char timebuf[32];
-            const auto tm = std::localtime(&st.st_mtime);
-            const auto sres = std::strftime(timebuf, sizeof(timebuf), "%Y-%m-%d %H:%M:%S", tm);
-            if (sres == 0) {
-                std::cerr << "Could not format modification time: " << st.st_mtime << std::endl;
-                return 3;
-            }
-            values.push_back(std::string(timebuf));
         }
-
-        output.row(values);
     }
-    ::closedir(dir);
+
     return 0;
 }
